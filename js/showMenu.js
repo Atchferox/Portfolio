4eed8a259f5c9b8a6ff862fa5727062fcb1fcebe
Luuk <Luukmunneke@gmail.com>
2022-06-20 19:09:28 +0200

    portfolio website

---
+++ js/showMenu.js
@@ -0,0 +1,33 @@
+function handleButtonClick() {
+
+  let navbar = document.getElementById("myLinks")
+  let main = document.querySelector("main")
+  let scrollContainer = document.querySelector(".scrolldown")
+
+  if (main.style.transform === '') {
+    main.style.transform = 'translateX(290px)';
+    fadeOut(main);
+    scrollContainer.style.visibility = "hidden";
+    navbar.style.transform = 'translateX(0%)';
+  } else {
+    main.style.transform = '';
+    navbar.style.transform = 'translateX(-100%)'
+    main.style.opacity = '1'
+    scrollContainer.style.visibility = "visible";
+
+  }
+}
+
+
+function fadeOut(main) {
+  let fadeEffect = setInterval(function () {
+    if (!main.style.opacity) {
+      main.style.opacity = 1;
+    }
+    if (main.style.opacity > 0.9) {
+      main.style.opacity -= 0.002;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, 1)
+}
